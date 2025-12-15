03a804482cf636202f82161fb24ea256a83ccd1e
folk23444 <Thanyaton.jv@gmail.com>
2025-12-15 17:37:22 +0700

    temp

--- src/q12/temp.ts
+++ src/q12/temp.ts
@@ -1,8 +1,7 @@
-const input5 = process.argv[2];
-if(process.argv[2] === undefined){
-  console.log("Please Enter @")
-  process.exit(1)}
-
-  if (input5 >= 30) {
-    console.log("Hot")
+const input5 = Number(process.argv[2]);
+  if(input5 > 30){
+    console.log("Hot");
+  }
+  else{
+    console.log('Normal')
   }
