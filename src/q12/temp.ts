80d8e913cbbe0e357be04641af81458aefcb5776
folk23444 <Thanyaton.jv@gmail.com>
2025-12-15 17:26:38 +0700

    Test2

---
+++ src/q12/temp.ts
@@ -0,0 +1,8 @@
+const input5 = process.argv[2];
+if(process.argv[2] === undefined){
+  console.log("Please Enter @")
+  process.exit(1)}
+
+  if (input5 >= 30) {
+    console.log("Hot")
+  }
---
+++ src/q14/Even.ts
@@ -0,0 +1,7 @@
+let number5 = Number(process.argv[2]);
+for (let i = 0; i <=number5 ; i++){
+if (i %2 !== 0) {
+continue
+}
+console.log(i)
+}
