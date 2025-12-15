e00b8aa5e3f5d9535d4d970510c2efc4610853ac
folk23444 <Thanyaton.jv@gmail.com>
2025-12-15 17:49:12 +0700

    light

---
+++ src/q13/light.ts
@@ -0,0 +1,12 @@
+if (process.argv[2] = "red"){
+console.log("Stop")
+}
+else if (process.argv[2] = "Red"){
+    console.log("Stop")
+}
+else if (process.argv[2] = "RED"){
+    console.log("Stop")
+}
+else {
+    console.log("Unknow")
+}
