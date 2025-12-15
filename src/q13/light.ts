c107a6061b702c4c413d5b02a57c376a7089fff0
folk23444 <Thanyaton.jv@gmail.com>
2025-12-15 18:06:08 +0700

    light

--- src/q13/light.ts
+++ src/q13/light.ts
@@ -7,6 +7,18 @@ else if (process.argv[2] = "Red"){
 else if (process.argv[2] = "RED"){
     console.log("Stop")
 }
+else if (process.argv[2] = "yellow"){
+    console.log("Caution")}
+else if (process.argv[2] = "Yellow"){
+    console.log("Caution")}
+else if (process.argv[2] = "YELLOW"){
+    console.log("Caution")}
+else if (process.argv[2] = "green"){
+    console.log("GO")}
+else if (process.argv[2] = "Green"){
+    console.log("GO")}
+else if (process.argv[2] = "GREEN"){
+    console.log("GO")}
 else {
-    console.log("Unknow")
+    console.log("Unknown")
 }
